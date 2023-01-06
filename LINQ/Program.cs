862c63a79c1a0b360ac392af9883b1ba1885f48c
Chanise Hart <niecy23@gmail.com>
2023-01-06 15:31:17 -0700

    Exercise 2 With Comments

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Creating a list and adding the game names.
+            var VideoGames = new List<string>()
+            {
+                "Mario Kart",
+                "Blur",
+                "Need For Speed",
+                "Call of Duty",
+                "Roblox",
+                "Sims"
+            };
+
+            //Creating a new list that is ordered by the length of the game names.
+            var OrderedList = VideoGames.OrderBy(name => name.Length);
+
+            //Printing the new list to the console.
+            Console.WriteLine("List in order by length of game name");
+            Console.WriteLine("----------"); 
+            foreach (var game in OrderedList)
+            {
+                Console.WriteLine($"{game}"); 
+            }
+
         }
     }
 }
